61b116e21c2525451c897d587841640839fad7ae
Breeze <starkunshao@qq.com>
2022-10-20 09:56:59 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/*
+@Author: kun
+@Date Created in 2022-10-20 9:53 
+*/
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}

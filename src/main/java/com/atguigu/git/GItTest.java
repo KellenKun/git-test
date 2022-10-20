9734c32b19e44c235c89b054e2545441e9316a3d
Breeze <starkunshao@qq.com>
2022-10-20 10:35:56 +0800

    master commit2

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -10,5 +10,6 @@ public class GItTest {
         System.out.println("hello git!!!22222222222");
         System.out.println("hello git!!!33333333333");
         System.out.println("hello git!!!444444444444");
+        System.out.println("master test");
     }
 }
